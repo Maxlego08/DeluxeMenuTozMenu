d484085c7c876f432e7c4f11ba790048c22c3630
Maxlego08 <maxlego08@sfr.fr>
2022-06-18 14:29:16 +0200

    :construction: Add permission and placeholder convert

--- src/fr/maxlego08/convert/ConvertDeluxeMenu.java
+++ src/fr/maxlego08/convert/ConvertDeluxeMenu.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -19,8 +20,14 @@ import org.bukkit.inventory.ItemFlag;
 
 import com.extendedclip.deluxemenus.menu.Menu;
 import com.extendedclip.deluxemenus.menu.MenuItem;
+import com.extendedclip.deluxemenus.requirement.HasPermissionRequirement;
+import com.extendedclip.deluxemenus.requirement.InputResultRequirement;
+import com.extendedclip.deluxemenus.requirement.Requirement;
+import com.extendedclip.deluxemenus.requirement.RequirementList;
+import com.extendedclip.deluxemenus.requirement.RequirementType;
 
 import fr.maxlego08.menu.MenuPlugin;
+import fr.maxlego08.menu.api.enums.PlaceholderAction;
 import fr.maxlego08.menu.zcore.logger.Logger;
 import fr.maxlego08.menu.zcore.utils.ZUtils;
 
@@ -115,9 +122,11 @@ public class ConvertDeluxeMenu extends ZUtils {
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
+
 			}
 
 			message(sender, "§aConversion complete. §7Please check that your files have been converted correctly.");
+			message(sender, "§7Dont forget to run §f/zmenu reload§7.");
 			this.isRunning = false;
 		});
 
@@ -134,21 +143,58 @@ public class ConvertDeluxeMenu extends ZUtils {
 		Map<Integer, TreeMap<Integer, MenuItem>> items = menu.getMenuItems();
 
 		items.forEach((value, treeMap) -> {
-			treeMap.forEach((value2, item) -> {
-				this.saveButton(item, configuration, "items." + value + ".");
-			});
+
+			String path = "items." + value + ".";
+
+			if (treeMap.size() == 1) {
+				MenuItem item = treeMap.firstEntry().getValue();
+				System.out.println(value + " - Slot: " + item.getSlot());
+				this.saveButton(item, configuration, path);
+			} else {
+				treeMap.forEach((value2, item) -> {
+					System.out.println(
+							value + " - " + value2 + " - Slot: " + item.getSlot() + " Priority: " + item.getPriority());
+					// this.saveButton(item, configuration, "items." + value +
+					// ".");
+				});
+
+				List<MenuItem> sortItems = treeMap.values().stream()
+						.sorted(Comparator.comparingInt(MenuItem::getPriority)).collect(Collectors.toList());
+
+				for (MenuItem item : sortItems) {
+					System.out.println(value + " - X - Slot: " + item.getSlot() + " Priority: " + item.getPriority());
+					this.saveButton(item, configuration, path);
+					path += "else.";
+				}
+			}
 		});
 
 	}
 
+	/**
+	 * Allows you to save a button
+	 * 
+	 * @param item
+	 * @param configuration
+	 * @param path
+	 */
 	private void saveButton(MenuItem item, YamlConfiguration configuration, String path) {
 
 		configuration.set(path + "type", "NONE");
 		configuration.set(path + "slot", item.getSlot());
+
+		this.saveViewRequirement(item, configuration, path);
 		this.saveItem(item, configuration, path + "item.");
 
 	}
 
+	/**
+	 * Allows you to save an item
+	 * 
+	 * @param item
+	 * @param configuration
+	 * @param path
+	 */
 	private void saveItem(MenuItem item, YamlConfiguration configuration, String path) {
 
 		if (item.getMaterial() != null) {
@@ -183,37 +229,38 @@ public class ConvertDeluxeMenu extends ZUtils {
 		this.saveFlags(item, configuration, path);
 	}
 
-	@SuppressWarnings({ "unchecked", "deprecation" })
+	@SuppressWarnings({ "deprecation" })
+	/**
+	 * Allows to save enchantments
+	 * 
+	 * @param item
+	 * @param configuration
+	 * @param path
+	 */
 	private void saveEnchantments(MenuItem item, YamlConfiguration configuration, String path) {
 
 		try {
-
-			Field field = null;
-			for (Field f : item.getClass().getDeclaredFields()) {
-				if (f.getName().equalsIgnoreCase("enchantments")) {
-					field = f;
-					break;
+			Map<Enchantment, Integer> enchantments = getField(item, "enchantments");
+			if (enchantments != null) {
+				List<String> list = enchantments.entrySet().stream().map(entry -> {
+					return entry.getKey().getName() + "," + entry.getValue();
+				}).collect(Collectors.toList());
+				if (list.size() > 0) {
+					configuration.set(path + "enchants", list);
 				}
 			}
-
-			if (field == null) {
-				return;
-			}
-
-			field = item.getClass().getField("enchantments");
-			field.setAccessible(true);
-			Map<Enchantment, Integer> enchantments = (Map<Enchantment, Integer>) field.get(item);
-			List<String> list = enchantments.entrySet().stream().map(entry -> {
-				return entry.getKey().getName() + "," + entry.getValue();
-			}).collect(Collectors.toList());
-			if (list.size() > 0) {
-				configuration.set(path + "enchants", list);
-			}
-		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * Allows to save the ItemFlags
+	 * 
+	 * @param item
+	 * @param configuration
+	 * @param path
+	 */
 	private void saveFlags(MenuItem item, YamlConfiguration configuration, String path) {
 
 		List<ItemFlag> flags = item.itemFlags() != null ? item.itemFlags() : new ArrayList<ItemFlag>();
@@ -234,7 +281,108 @@ public class ConvertDeluxeMenu extends ZUtils {
 			flags.add(ItemFlag.HIDE_UNBREAKABLE);
 		}
 
-		configuration.set(path + "flags", flags.stream().map(ItemFlag::name).collect(Collectors.toList()));
+		if (flags.size() > 0) {
+			configuration.set(path + "flags", flags.stream().map(ItemFlag::name).collect(Collectors.toList()));
+		}
+	}
+
+	/**
+	 * Allows you to save view requirements
+	 * 
+	 * @param item
+	 * @param configuration
+	 * @param path
+	 */
+	private void saveViewRequirement(MenuItem item, YamlConfiguration configuration, String path) {
+
+		RequirementList requirements = item.getViewRequirements();
+		if (requirements == null) {
+			return;
+		}
+
+		for (Requirement requirement : requirements.getRequirements()) {
+
+			if (requirement instanceof HasPermissionRequirement) {
+
+				try {
+					String permission = this.getField(requirement, "perm");
+					configuration.set(path + "permission", permission);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			} else if (requirement instanceof InputResultRequirement) {
+
+				try {
+
+					String input = getField(requirement, "input");
+					String result = getField(requirement, "result");
+					RequirementType type = getField(requirement, "type");
+
+					PlaceholderAction action = convertAction(type);
+					configuration.set(path + "action", action.name());
+					configuration.set(path + "placeHolder", input);
+					configuration.set(path + "value", result);
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+
+		}
+
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T> T getField(Object object, String fieldName) throws Exception {
+		Field field = object.getClass().getDeclaredField(fieldName);
+		field.setAccessible(true);
+		return (T) field.get(object);
+	}
+
+	private PlaceholderAction convertAction(RequirementType type) {
+		switch (type) {
+		case DOES_NOT_HAVE_EXP:
+		case DOES_NOT_HAVE_ITEM:
+		case DOES_NOT_HAVE_META:
+		case DOES_NOT_HAVE_MONEY:
+		case DOES_NOT_HAVE_PERMISSION:
+		case REGEX_DOES_NOT_MATCH:
+		case REGEX_MATCHES:
+		case HAS_EXP:
+		case HAS_ITEM:
+		case HAS_META:
+		case HAS_MONEY:
+		case HAS_PERMISSION:
+		case IS_NEAR:
+		case IS_NOT_NEAR:
+		case JAVASCRIPT:
+		case STRING_DOES_NOT_EQUAL:
+		case STRING_DOES_NOT_EQUAL_IGNORECASE:
+			break;
+		case EQUAL_TO:
+			return PlaceholderAction.EQUAL_TO;
+		case GREATER_THAN:
+			return PlaceholderAction.SUPERIOR;
+		case GREATER_THAN_EQUAL_TO:
+			return PlaceholderAction.SUPERIOR_OR_EQUAL;
+		case LESS_THAN:
+			return PlaceholderAction.LOWER;
+		case LESS_THAN_EQUAL_TO:
+			return PlaceholderAction.LOWER_OR_EQUAL;
+		case STRING_CONTAINS:
+			return PlaceholderAction.CONTAINS_STRING;
+		case STRING_DOES_NOT_CONTAIN:
+			break;
+		case STRING_EQUALS:
+			return PlaceholderAction.EQUALS_STRING;
+		case STRING_EQUALS_IGNORECASE:
+			return PlaceholderAction.EQUALSIGNORECASE_STRING;
+		default:
+			break;
+		}
+		return null;
 	}
 
 }
